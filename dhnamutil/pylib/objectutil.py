712f90d7ec65d72fb2a640dbc383428bf3525342
Daehwan Nam <dhnam@postech.ac.kr>
2021-07-06 02:32:00 +0900

    get_nested_attr and get_nested_item are added

--- dhnamutil/pylib/objectutil.py
+++ dhnamutil/pylib/objectutil.py
@@ -4,3 +4,23 @@ def getattr_or_default(obj, attr, default_value=None):
         return getattr(obj, attr)
     else:
         return default_value
+
+
+def get_nested_attr(obj, *attrs, default_value=None):
+    if len(attrs) == 1 and '.' in attrs[0]:
+        attrs = attrs[0].split('.')
+    for attr in attrs:
+        if hasattr(obj, attr):
+            obj = getattr(obj, attr)
+        else:
+            return default_value
+    return obj
+
+
+def get_nested_item(obj, *keys, default_value=None):
+    for key in keys:
+        if key in obj:
+            obj = obj[key]
+        else:
+            return default_value
+    return obj
