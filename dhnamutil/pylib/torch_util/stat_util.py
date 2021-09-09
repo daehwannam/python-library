2fd99172820dd859f3af05ba10454b5c9a763a74
Daehwan Nam <dhnam@postech.ac.kr>
2021-09-09 19:30:49 +0900

    stat_util is added

---
+++ dhnamutil/pylib/torch_util/stat_util.py
@@ -0,0 +1,27 @@
+
+# statistics utility
+
+import torch
+import torch.nn.functional as F
+
+
+def get_gumbels(self, num_gunbels):
+    # F^-1(u) = -log(-log(u))
+    # u ~ Uniform(0, 1)
+    return -torch.log(-torch.log(torch.rand(num_gunbels)))
+
+
+def get_perturbed_log_probs(self, parent_perturbed_log_prob, child_log_probs):
+    gumbels = self.get_gumbels(len(child_log_probs))
+    perturbed_log_probs = child_log_probs + gumbels
+
+    assert perturbed_log_probs.dim() == 1
+    max_child_perturbed_log_prob, _ = perturbed_log_probs.max(dim=0)
+
+    T = parent_perturbed_log_prob
+    Z = max_child_perturbed_log_prob
+
+    u = T - perturbed_log_probs + torch.log1p(- torch.exp(perturbed_log_probs - Z))
+    perturbed_log_probs = T - F.relu(u) - torch.log1p(torch.exp(-u.abs()))
+
+    return perturbed_log_probs, gumbels
