ab1ea6f55bac700c7baf24da9966a05a2d423411
Daehwan Nam <dhnam@postech.ac.kr>
2021-11-08 11:39:16 +0900

    LazyEval is added

---
+++ dhnamutil/pylib/lazyutil.py
@@ -0,0 +1,15 @@
+
+class LazyEval:
+    def __init__(self, fn, *args, **kwargs):
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.evaluated = False
+        self.obj = None
+
+    def get(self):
+        if not self.evaluated:
+            self.obj = self.fn(*self.args, **self.kwargs)
+            self.evaluated = True
+
+        return self.obj
