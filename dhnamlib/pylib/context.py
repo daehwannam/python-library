24b4f06f2684009330e74402fc126eedafd915b8
Daehwan Nam <dhnam@postech.ac.kr>
2023-06-26 04:13:06 +0900

    Scope is renamed as Environment

--- dhnamlib/pylib/context.py
+++ dhnamlib/pylib/context.py
@@ -1,43 +1,47 @@
 
 import inspect
 import functools
+import tempfile
+import shutil
+import os
 # from contextlib import contextmanager  # https://realpython.com/python-with-statement/#creating-function-based-context-managers
 
-from .lazy import LazyEval
+from .lazy import LazyEval, eval_if_lazy
 from .decoration import deprecated
+from . import filesys
 
-# Scope
+# Environment
 # modified from https://stackoverflow.com/a/2002140/6710003
 
-class Scope:
+class Environment:
     """
     Example:
 
     >>> # Set the default values
-    >>> scope = Scope(a=100)
+    >>> env = Environment(a=100)
     >>>
     >>> # Placeholders as default arguments
-    >>> # e.g. explicit naming -> scope.ph.c / implicit naming -> scope.ph
-    >>> @scope
-    ... def func(u, w, x=scope.ph.c, y=scope.ph, z=scope.ph):
+    >>> # e.g. explicit naming -> env.ph.c / implicit naming -> env.ph
+    >>> @env
+    ... def func(u, w, x=env.ph.c, y=env.ph, z=env.ph):
     ...     return u + w + x + y + z
     >>>
     >>> def my_sum(*args):
     ...    print('Calling my_sum')
     ...    return sum(args)
     >>>
-    >>> # Set values in a dynamic scope
-    >>> with scope(b=3, c=5, y=10, z=LazyEval(my_sum, 1, 2, 3, 4, 5)):
+    >>> # Set values in a dynamic env
+    >>> with env(b=3, c=5, y=10, z=LazyEval(my_sum, 1, 2, 3, 4, 5)):
     ...     print('Before the 1st function call')
-    ...     print(func(u=scope.a, w=scope.b))
+    ...     print(func(u=env.a, w=env.b))
     ...     print('Before the 2nd function call')
-    ...     print(func(u=scope.a, w=scope.b))
-    ...     # Override value fo 'a' in the inner scope
-    ...     with scope(a=1000):
+    ...     print(func(u=env.a, w=env.b))
+    ...     # Override value fo 'a' in the inner env
+    ...     with env(a=1000):
     ...         print('Before the 3rd function call')
-    ...         print(func(u=scope.a, w=scope.b))
+    ...         print(func(u=env.a, w=env.b))
     ...     print('Before the 4th function call')
-    ...     print(func(u=scope.a, w=scope.b))
+    ...     print(func(u=env.a, w=env.b))
     Before the 1st function call
     Calling my_sum
     133
@@ -66,41 +70,48 @@ class Scope:
     def __getattr__(self, name):
         if name in self._reserved_names:
             raise Exception(_reserved_name_exceptoin_format_str.format(name=name))
-        for scope in reversed(self._stack):
-            if name in scope:
-                value = scope[name]
-                if isinstance(value, LazyEval):
-                    return value.get()
-                else:
-                    return value
-        raise AttributeError(f'no such variable "{name}" in the scope')
+        for local_env in reversed(self._stack):
+            if name in local_env:
+                value = local_env[name]
+                return eval_if_lazy(value)
+        raise AttributeError(f'no such variable "{name}" in the local_env')
 
     def __setattr__(self, name, value):
         if self._setattr_enabled:
             super().__setattr__(name, value)
         else:
-            # raise AttributeError("scope variables can only be set by `with Scope.let()` or `Scope.update`")
-            raise AttributeError("scope variables can only be set by `with Scope.let()`")
+            # raise AttributeError("env variables can only be set by `with Environment.let()` or `Environment.update`")
+            raise AttributeError("Attributes can only be set by `with Environment.let()`")
 
     def let(self, pairs=(), **kwargs):
-        # context manager for a dynamic scope
+        # context manager for a dynamic env
         for reserved_name in self._reserved_names:
             if reserved_name in kwargs:
                 raise Exception(_reserved_name_exceptoin_format_str.format(name=reserved_name))
 
-        return _ScopeBlock(self._stack, pairs, kwargs)
+        return _EnvironmentBlock(self._stack, pairs, kwargs)
 
     @deprecated
     def update(self, pairs=(), **kwargs):
         self._stack[-1].update(pairs, **kwargs)
 
-    def items(self):
+    def items(self, lazy=True):
         keys = set()
-        for scope in reversed(self._stack):
-            for k, v in scope.items():
-                if k not in keys:
-                    keys.add(k)
-                    yield k, v
+        for local_env in reversed(self._stack):
+            for key, value in local_env.items():
+                if key not in keys:
+                    keys.add(key)
+                    if lazy:
+                        yield key, value
+                    else:
+                        yield key, eval_if_lazy(value)
+
+    def __contains__(self, name):
+        for local_env in reversed(self._stack):
+            if name in local_env:
+                return True
+        else:
+            return False
 
     def decorate(self, func):
         signature = inspect.signature(func)
@@ -111,7 +122,7 @@ class Scope:
                         param.default is not inspect.Parameter.empty and
                         (isinstance(param.default, _Placeholder) or
                          isinstance(param.default, _PlaceholderFactory)) and
-                        param.default.scope == self
+                        param.default.env == self
                 ):
                     if param.kind == inspect._ParameterKind.POSITIONAL_OR_KEYWORD:
                         pos_idx = idx
@@ -156,7 +167,7 @@ class Scope:
 _reserved_name_exceptoin_format_str = '"{name} is a reserved name'
 
 
-class _ScopeBlock:
+class _EnvironmentBlock:
     def __init__(self, stack, pairs, kwargs):
         self._stack = stack
         self._dict = dict(pairs, **kwargs)
@@ -169,19 +180,19 @@ class _ScopeBlock:
 
 
 class _PlaceholderFactory:
-    def __init__(self, scope: Scope):
-        self.scope = scope
+    def __init__(self, env: Environment):
+        self.env = env
 
     def __getattr__(self, name):
-        return _Placeholder(self.scope, name)
+        return _Placeholder(self.env, name)
 
 class _Placeholder:
-    def __init__(self, scope, name):
-        self.scope = scope
+    def __init__(self, env, name):
+        self.env = env
         self.name = name
 
     def get_value(self):
-        return self.scope.__getattr__(self.name)
+        return self.env.__getattr__(self.name)
 
 
 # block
@@ -211,3 +222,58 @@ class _Block:
 
 
 block = _Block()
+
+
+class _ReplaceDirectory:
+    def __init__(self, dir_path, force=False):
+        self.dir_path = dir_path
+        self.force = force
+
+    def __enter__(self):
+        if not os.isdir(self.dir_path):
+            if os.isfile(self.dir_path):
+                raise Exception(f'"{self.dir_path}" is a file rather than a directory')
+            elif self.force:
+                os.makedirs(self.dir_path)
+            else:
+                raise Exception(f'"{self.dir_path}" does not exist')
+        parent_dir_path = filesys.get_parent_path(self.dir_path)
+        self.temp_dir_path = tempfile.mkdtemp(dir=parent_dir_path)
+        dir_octal_mode = filesys.get_octal_mode(self.temp_dir_path)
+        filesys.set_octal_mode(self.temp_dir_path, dir_octal_mode)
+        return self.temp_dir_path
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        shutil.rmtree(self.dir_path)
+        os.rename(self.temp_dir_path, self.dir_path)
+
+
+def replace_dir(dir_path, force=False):
+    '''
+    :param dir_path: The path to a directory
+    :param force: If force=True, ignore existence of the directory. Otherwise raise exception.
+
+    Example:
+
+    >>> dir_path = 'some-dir'
+    >>> os.makedirs(dir_path)
+    >>> with open(os.path.join(dir_path, 'some-file-1'), 'w') as f:
+    ...     pass
+    ...
+    >>> os.listdir(dir_path)
+    ['some-file-1']
+    >>> with replace_dir(dir_path) as temp_dir_path:
+    ...     with open(os.path.join(temp_dir_path, 'some-file-2'), 'w') as f:
+    ...         pass
+    ...
+    >>> os.listdir(dir_path)
+    ['some-file-2']
+    >>> shutil.rmtree(dir_path)  # remove the directory
+    '''
+    return _ReplaceDirectory(dir_path, force=force)
+
+
+def copy_dir(source, target, replacing=False, overwriting=False):
+    if replacing:
+        shutil.rmtree(target)
+    return shutil.copytree(source, target, dirs_exist_ok=overwriting)
