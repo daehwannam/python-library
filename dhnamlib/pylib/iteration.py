c8350992c4ca71b5efe39df7598e7ed1278d2b8d
Daehwan Nam <dhnam@postech.ac.kr>
2024-06-02 08:55:22 +0900

    fixing not_none_valued_dict

--- dhnamlib/pylib/iteration.py
+++ dhnamlib/pylib/iteration.py
@@ -229,7 +229,7 @@ def not_none_valued_dict(pairs=(), **kwargs):
     >>> not_none_valued_dict(a=10, b=None, c=20)
     {'a': 10, 'c': 20}
     '''
-    if len(pairs) == 1 and isinstance(pairs, dict):
+    if isinstance(pairs, dict):
         _pairs = pairs.items()
     else:
         _pairs = pairs
